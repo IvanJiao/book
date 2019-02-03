c59706431c21ce9b023da88848d2e4593a1d2583
ivanjiao <jiaoxiaoa@163.com>
2019-02-04 00:16:51 +0800

    start

---
+++ src/main/java/com/sousouit/entity/Book.java
@@ -0,0 +1,96 @@
+package com.sousouit.entity;
+
+public class Book {
+
+    private String bookid;
+    private String bookname;
+    private String bkfilename;
+    private String bkauthor;
+    private String bktype;
+    private String bksize;
+    private String bkpublictime;
+    private String bkpublicname;
+    private String downtimes;
+    private String points;
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public String getBkfilename() {
+        return bkfilename;
+    }
+
+    public String getBkauthor() {
+        return bkauthor;
+    }
+
+    public String getBktype() {
+        return bktype;
+    }
+
+    public String getBksize() {
+        return bksize;
+    }
+
+    public String getBkpublictime() {
+        return bkpublictime;
+    }
+
+    public String getBkpublicname() {
+        return bkpublicname;
+    }
+
+    public String getDowntimes() {
+        return downtimes;
+    }
+
+    public void setBookid(String bookid) {
+        this.bookid = bookid;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public void setBkfilename(String bkfilename) {
+        this.bkfilename = bkfilename;
+    }
+
+    public void setBkauthor(String bkauthor) {
+        this.bkauthor = bkauthor;
+    }
+
+    public void setBktype(String bktype) {
+        this.bktype = bktype;
+    }
+
+    public void setBksize(String bksize) {
+        this.bksize = bksize;
+    }
+
+    public void setBkpublictime(String bkpublictime) {
+        this.bkpublictime = bkpublictime;
+    }
+
+    public void setBkpublicname(String bkpublicname) {
+        this.bkpublicname = bkpublicname;
+    }
+
+    public void setDowntimes(String downtimes) {
+        this.downtimes = downtimes;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public String getBookid() {
+        return bookid;
+
+    }
+}
--- src/main/java/com/sousouit/entity/User.java
+++ src/main/java/com/sousouit/entity/User.java
@@ -1,4 +1,62 @@
 package com.sousouit.entity;
 
+import java.util.List;
+import java.util.Map;
+
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String points;
+    private List<String> hisbooks;
+    private Map<String, String> hisprocess;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public void setHisbooks(List<String> hisbooks) {
+        this.hisbooks = hisbooks;
+    }
+
+    public void setHisprocess(Map<String, String> hisprocess) {
+        this.hisprocess = hisprocess;
+    }
+
+    public String getUsername() {
+
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public List<String> getHisbooks() {
+        return hisbooks;
+    }
+
+    public Map<String, String> getHisprocess() {
+        return hisprocess;
+    }
 }
